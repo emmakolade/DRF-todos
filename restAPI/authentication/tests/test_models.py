3c38861ecd37742b3b29b58dfeb1a77656189af5
Akolade <emmakolade@gmail.com>
2023-01-02 08:43:00 +0100

    generic Api View

--- restAPI/authentication/tests/test_models.py
+++ restAPI/authentication/tests/test_models.py
@@ -1,6 +1,48 @@
 from rest_framework.test import APITestCase
+from authentication.models import User
 
 
 class TestModel(APITestCase):
     def test_create_user(self):
-        self.assertEqual(1, 1-0)
+        user = User.objects.create_user(
+            'akolade', 'emmakolade@gmail.com', 'Password123##')  # checks if users can be created
+        self.assertIsInstance(user, User)
+        self.assertFalse(user.is_staff)
+        self.assertEqual(user.email, 'emmakolade@gmail.com')
+
+    def test_create_super_user(self):
+        user = User.objects.create_superuser(
+            'akolade', 'emmakolade@gmail.com', 'Password123##')  # checks if users can be created
+        self.assertIsInstance(user, User)
+        self.assertTrue(user.is_staff)
+        self.assertEqual(user.email, 'emmakolade@gmail.com')
+
+    def test_raises_error_when_no_username_is_supplied(self):
+        self.assertRaises(ValueError, User.objects.create_user, username='',
+                          email='emmakolade@gmail.com', password='Password123##')
+        self.assertRaisesMessage(ValueError, "The given username must be set")
+
+    def test_raises_error_message_when_no_username_is_supplied(self):
+        with self.assertRaisesMessage(ValueError, "The given username must be set"):
+            User.objects.create_user(
+                username='', email='emmakolade@gmail.com', password='Password123##')
+
+    def test_raises_error_when_no_email_is_supplied(self):
+        self.assertRaises(ValueError, User.objects.create_user,
+                          username='akolade', email='', password='Password123##')
+        self.assertRaisesMessage(ValueError, "The given email must be set")
+
+    def test_raises_error_message_when_no_email_is_supplied(self):
+        with self.assertRaisesMessage(ValueError, "The given email must be set"):
+            User.objects.create_user(
+                username='akolade', email='', password='Password123##')
+
+    def test_cant_create_super_user_with_no_is_staff_status(self):
+        with self.assertRaisesMessage(ValueError, "Superuser must have is_staff=True."):
+            User.objects.create_superuser(
+                username='akolade', email='emmakolade@gmail.com', password='Password123##', is_staff=False)
+
+    def test_cant_create_super_user_with_no_super_user_status(self):
+        with self.assertRaisesMessage(ValueError, "Superuser must have is_superuser=True."):
+            User.objects.create_superuser(
+                username='akolade', email='emmakolade@gmail.com', password='Password123##', is_superuser=False)
--- restAPI/authentication/views.py
+++ restAPI/authentication/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from rest_framework.generics import GenericAPIView
 
-# Create your views here.
+
+class RegisterAPIView(GenericAPIView):
+    def post(self, request):
+        pass
