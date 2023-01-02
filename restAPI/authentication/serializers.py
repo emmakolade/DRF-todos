5e01c1e0475340cb81b93ccb764d78db8d3bab99
Akolade <emmakolade@gmail.com>
2023-01-02 09:07:52 +0100

    serailizers

--- restAPI/authentication/serializers.py
+++ restAPI/authentication/serializers.py
@@ -1,1 +1,14 @@
 from rest_framework import serializers
+from authentication.models import User
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(
+        max_length=150, min_length=8, write_only=True)
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password',)
+    
+    def create(self, validated_data):
+        return User.objects.create_user(**validated_data)
---
+++ restAPI/authentication/urls.py
@@ -0,0 +1,7 @@
+from authentication import views
+from django.urls import path
+
+
+urlpatterns = [
+    path('register', views.RegisterAPIView.as_view(), name="register")
+]
