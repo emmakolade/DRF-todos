a9d343fa9a139a48845c2e4b5d6d528020d22e5f
Akolade <emmakolade@gmail.com>
2023-01-05 05:22:27 +0100

    serializers in todos

--- restAPI/todos/views.py
+++ restAPI/todos/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from rest_framework.generics import CreateAPIView
+from todos.serializers import TodoSerializer
 # Create your views here.
 
 class CreateTodoApiView(CreateAPIView):
+    serializer_class=
