9ea5cdec5c092176463814e71165e8fdd731069d
Akolade <emmakolade@gmail.com>
2023-01-05 05:15:15 +0100

    todos model

--- restAPI/authentication/views.py
+++ restAPI/authentication/views.py
@@ -5,13 +5,13 @@ from rest_framework import response, status, permissions
 from django.contrib.auth import authenticate
 
 
-
 class RegisterAPIView(GenericAPIView):
+    authentication_classes = []
     serializer_class = RegisterSerializer
 
     def post(self, request):
         serializer = self.serializer_class(data=request.data)
-        
+
         if serializer.is_valid():
             serializer.save()
             return response.Response(serializer.data, status=status.HTTP_201_CREATED)
@@ -19,22 +19,24 @@ class RegisterAPIView(GenericAPIView):
 
 
 class LoginAPIView(GenericAPIView):
+    authentication_classes = []
     serializer_class = LoginSerializer
-    
+
     def post(self, request):
         email = request.data.get('email', None)
         password = request.data.get('password', None)
 
         user = authenticate(username=email, password=password)
-        
+
         if user:
-            serializer= self.serializer_class(user)
+            serializer = self.serializer_class(user)
             return response.Response(serializer.data, status=status.HTTP_200_OK)
         return response.Response({'message': 'invalid credentials, try again'}, status=status.HTTP_401_UNAUTHORIZED)
-            
+
 
 class AuthUserAPIView(GenericAPIView):
     permission_classes = (permissions.IsAuthenticated)
+
     def get(self, request):
         user = request.user
         serializer = RegisterSerializer(user)
--- restAPI/todos/models.py
+++ restAPI/todos/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from helpers.models import TrackingModel
+from  authentication.models import User
 
-# Create your models here.
+
+class Todo(TrackingModel):
+    user=models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=250)
+    descriptio = models.TextField()
+    is_complete=models.BooleanField(default=False)
+    
+    
+    def __str__(self):
+        return self.title
+    
+    
+    
