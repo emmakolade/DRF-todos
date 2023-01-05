09717c61fa6db2e4693c9c6238443af1cd5cd906
Akolade <emmakolade@gmail.com>
2023-01-05 04:52:47 +0100

    JWT auth

--- restAPI/restAPI/settings.py
+++ restAPI/restAPI/settings.py
@@ -130,3 +130,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'authentication.jwt.JWTAuthentication',
+    ]
+}
